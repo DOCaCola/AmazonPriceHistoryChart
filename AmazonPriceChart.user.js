4392bdf432dcf0f14e19f4d09ee1a8df84198566
Felix Freitag <felix.freitag@gmail.com>
2016-09-15 20:20:01 +0200

    Converted to ES6 via lebab

--- AmazonPriceChart.user.js
+++ AmazonPriceChart.user.js
@@ -12,28 +12,28 @@
 // ==/UserScript==
 
 
-var width = 800;
-var height = 250;
-var chart = "amazon-new-used"; //Possible other values are "amazon", "new", "used", "new-used", & "amazon-new-used"
+const width = 800;
+const height = 250;
+const chart = "amazon-new-used"; //Possible other values are "amazon", "new", "used", "new-used", & "amazon-new-used"
 
-var element = $(':input[id="ASIN"]');
-var asin = $.trim(element.attr("value"));
+const element = $(':input[id="ASIN"]');
+const asin = $.trim(element.attr("value"));
 
 if (asin.length) {
     // Retrieve top level domain name to determine correct country subdomain for camelcamelcamel
-    var arr = document.domain.split(".");
-    var country = arr[arr.length - 1];
+    const arr = document.domain.split(".");
+    let country = arr[arr.length - 1];
     if (country=="com") {country = "us";}
 
-    var link2 = "<a  target='blank' href='http://" + country + ".camelcamelcamel.com/product/" + asin + "'><img src='http://charts.camelcamelcamel.com/" + country + "/" + asin + "/" + chart + ".png?force=1&zero=0&w=" + width + "&h=" + height + "&desired=false&legend=1&ilt=1&tp=all&fo=0' /></a>";
-    var camelurl = 'http://' + country + '.camelcamelcamel.com/product/' + asin;
+    const link2 = `<a  target='blank' href='http://${country}.camelcamelcamel.com/product/${asin}'><img src='http://charts.camelcamelcamel.com/${country}/${asin}/${chart}.png?force=1&zero=0&w=${width}&h=${height}&desired=false&legend=1&ilt=1&tp=all&fo=0' /></a>`;
+    const camelurl = `http://${country}.camelcamelcamel.com/product/${asin}`;
     GM_xmlhttpRequest({
         method: 'GET',
         url: camelurl,
-        onload: function(response) {
-            var innerHTML = "<div id='camelcamelcamel content pdClearfix' style='margin: 10px;'>" + link2 + "</div>";
+        onload(response) {
+            const innerHTML = `<div id='camelcamelcamel content pdClearfix' style='margin: 10px;'>${link2}</div>`;
 
-            var insertHTML = "<div class=\"bucket\"><div class=\"a-divider a-divider-section\"></div><h2 class=\"default\">Price history</h2>";
+            let insertHTML = "<div class=\"bucket\"><div class=\"a-divider a-divider-section\"></div><h2 class=\"default\">Price history</h2>";
             insertHTML += innerHTML;
             insertHTML += "</div>";
             insertHTML += "<hr size=\"1\" noshade=\"noshade\" class=\"bucketDivider\">";
@@ -48,4 +48,3 @@ if (asin.length) {
         }
     });
 }
-
