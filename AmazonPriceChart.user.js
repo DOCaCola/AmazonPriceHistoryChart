c2249eb661c54c1ab1f5cb36c3c27d37934460a3
Felix Freitag <felix.freitag@gmail.com>
2016-09-15 20:27:25 +0200

    Further code cleanup

--- AmazonPriceChart.user.js
+++ AmazonPriceChart.user.js
@@ -14,37 +14,42 @@
 
 const width = 800;
 const height = 250;
-const chart = "amazon-new-used"; //Possible other values are "amazon", "new", "used", "new-used", & "amazon-new-used"
+const chart = 'amazon-new-used'; //Possible other values are "amazon", "new", "used", "new-used", & "amazon-new-used"
 
-const element = $(':input[id="ASIN"]');
-const asin = $.trim(element.attr("value"));
+const $element = $(':input[id="ASIN"]');
+const asin = $.trim($element.attr('value'));
 
 if (asin.length) {
     // Retrieve top level domain name to determine correct country subdomain for camelcamelcamel
-    const arr = document.domain.split(".");
+    const arr = document.domain.split('.');
     let country = arr[arr.length - 1];
-    if (country=="com") {country = "us";}
+    if (country=='com') {country = 'us';}
 
-    const link2 = `<a  target='blank' href='http://${country}.camelcamelcamel.com/product/${asin}'><img src='http://charts.camelcamelcamel.com/${country}/${asin}/${chart}.png?force=1&zero=0&w=${width}&h=${height}&desired=false&legend=1&ilt=1&tp=all&fo=0' /></a>`;
+    const link2 = `
+<a  target="blank" href="http://${country}.camelcamelcamel.com/product/${asin}">
+    <img src="http://charts.camelcamelcamel.com/${country}/${asin}/${chart}.png?force=1&zero=0&w=${width}&h=${height}&desired=false&legend=1&ilt=1&tp=all&fo=0">
+</a>`;
     const camelurl = `http://${country}.camelcamelcamel.com/product/${asin}`;
     GM_xmlhttpRequest({
         method: 'GET',
         url: camelurl,
         onload(response) {
-            const innerHTML = `<div id='camelcamelcamel content pdClearfix' style='margin: 10px;'>${link2}</div>`;
+            const insertHTML = `
+<div class="bucket">
+  <div class="a-divider a-divider-section"></div>
+  <h2 class="default">Price history</h2>$
+  <div id="camelcamelcamel content pdClearfix" style="margin: 10px">
+    ${link2}
+  </div>
+</div>
+<hr size="1" noshade="noshade" class="bucketDivider">`;
 
-            let insertHTML = "<div class=\"bucket\"><div class=\"a-divider a-divider-section\"></div><h2 class=\"default\">Price history</h2>";
-            insertHTML += innerHTML;
-            insertHTML += "</div>";
-            insertHTML += "<hr size=\"1\" noshade=\"noshade\" class=\"bucketDivider\">";
-
-            if ($("#descriptionAndDetails").length) {
-                $("#descriptionAndDetails").prepend(insertHTML);
+            if ($('#descriptionAndDetails').length) {
+                $('#descriptionAndDetails').prepend(insertHTML);
             }
-            else if ($("#ask-btf_feature_div").length) {
-                $("#ask-btf_feature_div").prepend(insertHTML);
+            else if ($('#ask-btf_feature_div').length) {
+                $('#ask-btf_feature_div').prepend(insertHTML);
             }
-
         }
     });
 }
